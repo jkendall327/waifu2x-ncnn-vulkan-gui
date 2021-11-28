8a1a77681c48f5b007b2ab0e0850ee4b77ee9f5b
jackkendall <jkendall3096@gmail.com>
2021-11-28 08:09:35 +0000

    Initial commit

---
+++ Waifu2x-UI.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Waifu2x_UI.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
