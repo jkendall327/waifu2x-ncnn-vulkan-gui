a6b24c1e3ee8369d073d638e31b98fc6b46e134b
jackkendall <jkendall3096@gmail.com>
2021-11-28 16:26:28 +0000

    Clean up viewmodel

--- Waifu2x-UI.Avalonia/ViewModels/MainWindowViewModel.cs
+++ Waifu2x-UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -15,7 +15,6 @@ namespace Waifu2x_UI.Avalonia.ViewModels
         
         [Reactive] public string CurrentCommand { get; set; }
         [Reactive] public string ExecutableFilepath { get; set; }
-        [Reactive] public string SelectExecutablePrompt { get; set; }
         public ReactiveCommand<Unit, Unit> RunCommandCommand { get; }
         public ReactiveCommand<Unit, Unit> PickExecutableCommand { get; }
         public Interaction<Unit, string?> OpenFindExecutableDialog { get; } = new();
@@ -24,33 +23,10 @@ namespace Waifu2x_UI.Avalonia.ViewModels
         {
             _runner = runner;
             
-            SetupExecutablePrompt();
-
             var canExecute = StringHasValue(x => x.CurrentCommand);
             
             RunCommandCommand = ReactiveCommand.Create(RunCommand, canExecute);
             PickExecutableCommand = ReactiveCommand.CreateFromTask(PickExecutable);
-        }
-
-        private IObservable<bool> GetCanExecute()
-        {
-            return this.WhenAnyValue(
-                viewModel => viewModel.CurrentCommand,
-                currentCommand => !string.IsNullOrEmpty(currentCommand));
-        }
-
-        private void SetupExecutablePrompt()
-        {
-            void OnNext(bool isEmpty)
-            {
-                SelectExecutablePrompt = isEmpty ? string.Empty : "Please select your Waifu2X executable file.";
-            }
-
-            StringHasValue(x => x.ExecutableFilepath)
-                .Subscribe(
-                    OnNext,
-                    exception => throw exception,
-                    () => throw new InvalidOperationException());
         }
 
         private IObservable<bool> StringHasValue(Expression<Func<MainWindowViewModel,string>> expression)
