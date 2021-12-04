727d1c4c2e5384b7aa44b6222104ae19c8ebceea
jackkendall <jkendall3096@gmail.com>
2021-12-04 14:25:53 +0000

    Use Microsoft DI

--- Waifu2x-UI.Avalonia/App.axaml.cs
+++ Waifu2x-UI.Avalonia/App.axaml.cs
@@ -1,7 +1,9 @@
+using System;
 using System.IO.Abstractions;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 using Waifu2x_UI.Core;
 using Waifu2xUI.Avalonia.ViewModels;
 using Waifu2xUI.Avalonia.Views;
@@ -17,20 +19,34 @@ public class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var filesystem = new FileSystem();
-        var directory = filesystem.Directory;
-        
-        var runner = new CommandRunner(directory);
+        var provider = BuildServiceProvider();
+
+        var mainWindow = provider.GetRequiredService<MainWindow>();
         
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                Directory = filesystem.Directory,
-                DataContext = new MainWindowViewModel(runner, directory)
-            };
+            desktop.MainWindow = mainWindow;
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static IServiceProvider BuildServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        var filesystem = new FileSystem();
+
+        services.AddSingleton(filesystem.Directory);
+        services.AddTransient<ICommandRunner, CommandRunner>();
+        services.AddTransient<MainWindowViewModel>();
+
+        services.AddTransient(s => new MainWindow
+        {
+            Directory = s.GetRequiredService<IDirectory>(),
+            DataContext = s.GetRequiredService<MainWindowViewModel>()
+        });
+
+        return services.BuildServiceProvider();
     }
 }
