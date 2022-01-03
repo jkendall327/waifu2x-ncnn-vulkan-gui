9071d55039351496791d9ab5e5eb01428dc2c026
jackkendall <jkendall3096@gmail.com>
2022-01-03 09:46:19 +0000

    Pretty-print config when serializing by default

--- Waifu2x-UI.Core/Serialization/SerializationOptions.cs
+++ Waifu2x-UI.Core/Serialization/SerializationOptions.cs
@@ -3,5 +3,5 @@ namespace Waifu2x_UI.Core.Serialization;
 public class SerializationOptions
 {
     public bool SerializationEnabled { get; set; } = true;
-    public bool PrettyPrint { get; set; }
+    public bool PrettyPrint { get; set; } = true;
 }
